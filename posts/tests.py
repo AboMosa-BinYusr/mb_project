6852c6cd8cabf8979fe5573d0d7add604c2054e1
AboMosa <3byed393@gmail.com>
2022-06-17 18:42:53 +0200

    Tarnished

--- posts/tests.py
+++ posts/tests.py
@@ -1,14 +1,14 @@
 from django.test import TestCase
+from django.urls import reverse
 from .models import Post
 
 
 class PostModelTest(TestCase):
 
-    def setup(self):
+    def setUp(self):
         Post.objects.create(text='just a test')
     
     def test_text_content(self):
-        self.setup()
         post = Post.objects.get(id=1)
         expected_object_name = f'{post.text}'
-        self.assertEqual(expected_object_name, 'just a test')
+        self.assertEqual(expected_object_name, 'just a test')    
